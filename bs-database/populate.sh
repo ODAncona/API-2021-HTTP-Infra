0d8120b55877347032a6683b760c891b5c7a6aaf
Olivier D'Ancona <olivier.dancona@heig-vd.ch>
2022-01-04 09:25:28 +0100

    data

--- bs-database/populate.sh
+++ bs-database/populate.sh
@@ -1,4 +1,7 @@
 echo "Importation"
-mongoimport --db bs --collection users --drop --file data/users --jsonArray
 mongoimport --db bs --collection menus --drop --file data/menus --jsonArray
+mongoimport --db bs --collection dailymenus --drop --file data/dailymenus --jsonArray
+mongoimport --db bs --collection promotions --drop --file data/promotions --jsonArray
+mongoimport --db bs --collection reviews --drop --file data/reviews --jsonArray
+mongoimport --db bs --collection users --drop --file data/users --jsonArray
 echo "Importation terminée"
