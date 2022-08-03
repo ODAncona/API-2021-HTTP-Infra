e3d0147a3e2a5e9a75f84e76c7403bd596eadb81
Olivier D'Ancona <olivier.dancona@heig-vd.ch>
2022-08-03 14:11:19 +0200

    update 3.09.22

--- bs-website/src/app/bs-home/bs-home.component.ts
+++ bs-website/src/app/bs-home/bs-home.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-bs-home',
   templateUrl: './bs-home.component.html',
   styleUrls: ['./bs-home.component.scss'],
+  providers: [NgbCarouselConfig],
 })
 export class BsHomeComponent {
   images = [
@@ -11,5 +13,12 @@ export class BsHomeComponent {
     '../../assets/images/photos/Hotel_Beau_Suiza_SlideHome1.jpg',
   ];
 
-  constructor() {}
+  constructor(config: NgbCarouselConfig) {
+    config.showNavigationArrows = true;
+    config.showNavigationIndicators = true;
+    config.interval = 3000;
+    config.wrap = false;
+    config.keyboard = true;
+    config.pauseOnHover = true;
+  }
 }
