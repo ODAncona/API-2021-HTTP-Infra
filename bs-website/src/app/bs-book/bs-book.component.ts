10339265e50df8c1a546f051ded466e154639d84
Olivier D'Ancona <olivier.dancona@heig-vd.ch>
2022-08-06 16:16:46 +0200

    bs-book layout

--- bs-website/src/app/bs-book/bs-book.component.ts
+++ bs-website/src/app/bs-book/bs-book.component.ts
@@ -1,34 +1,22 @@
 import { Component, OnInit } from '@angular/core';
-import { UntypedFormGroup, UntypedFormControl, UntypedFormBuilder, Validators } from '@angular/forms';
-import { BreakpointObserver } from '@angular/cdk/layout';
-import { StepperOrientation } from '@angular/material/stepper';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { HttpClient } from '@angular/common/http';
+import {
+  UntypedFormGroup,
+  UntypedFormControl,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-bs-book',
   templateUrl: './bs-book.component.html',
-  styleUrls: ['./bs-book.component.scss']
+  styleUrls: ['./bs-book.component.scss'],
 })
 export class BsBookComponent {
-  stepperOrientation: Observable<StepperOrientation>;
   range = new UntypedFormGroup({
     start: new UntypedFormControl(),
-    end: new UntypedFormControl()
+    end: new UntypedFormControl(),
   });
-  firstFormGroup = this._formBuilder.group({
-    firstCtrl: ['', Validators.required]
-  });
-  secondFormGroup = this._formBuilder.group({
-    secondCtrl: ['', Validators.required]
-  });
-  thirdFormGroup = this._formBuilder.group({
-    thirdCtrl: ['', Validators.required]
-  });
-  constructor(private _formBuilder: UntypedFormBuilder, breakpointObserver: BreakpointObserver) {
-    this.stepperOrientation = breakpointObserver.observe('(min-width: 800px)').pipe(
-      map(({ matches }) => matches ? 'horizontal' : 'vertical')
-    );
-  }
-
+  bookingLink: string =
+    'https://live.ipms247.com/booking/book-rooms-hotelbeausite';
+  constructor(private httpClient:HttpClient) {}
 }
