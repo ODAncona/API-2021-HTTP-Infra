8b34419e022b6c19ebb70f246d1caad6126279ca
Olivier D'Ancona <olivier.dancona@heig-vd.ch>
2022-08-06 19:25:37 +0200

    bs-wellness good

--- bs-website/src/app/bs-wellness-wellness/bs-wellness-wellness.component.ts
+++ bs-website/src/app/bs-wellness-wellness/bs-wellness-wellness.component.ts
@@ -7,4 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BsWellnessWellnessComponent {
   constructor() {}
+  massages: any = [
+    { name: 'Back Massage', price: '	30min 60.- // 60min 110.-' },
+    { name: 'Full Body Massage', price: '60min 110.- 90min // 160.-' },
+    { name: 'Head & Face: Massage', price: '25min 50.- // 45min 80.-' },
+    { name: 'Hot Stone Massage', price: '30min 75.- // 60min 140.-' },
+    { name: 'Foot Massage: ', price: '30min 60.- ' },
+    {
+      name: 'Foot Massage + Peeling + Mask',
+      price: '60min 130.-',
+    },
+    { name: 'Lymphatic Drainage:', price: '30min 60.- // 60min 110.-' },
+  ];
 }
