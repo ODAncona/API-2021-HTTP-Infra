c0158624c1db537927c8a4a34aad08e40bcd65f4
Olivier D'Ancona <olivier.dancona@heig-vd.ch>
2022-08-12 13:44:04 +0200

    uniformisation

--- bs-website/src/app/bs-home/bs-home.component.ts
+++ bs-website/src/app/bs-home/bs-home.component.ts
@@ -27,7 +27,7 @@ export class BsHomeComponent {
       title: $localize`Rooms`,
       url: '/rooms',
       img: '../../assets/images/home/card_rooms.webp',
-      description: $localize`Discover`
+      description: $localize`Feel at home`
     },
     {
       title: $localize`Restaurant`,
@@ -37,27 +37,27 @@ export class BsHomeComponent {
     },
     {
       title: $localize`Promotions`,
-      url: '/promotion',
+      url: '/promotions',
       img: '../../assets/images/home/card_promotions.webp',
-      description: $localize`Discover `
+      description: $localize`Discover the best`
     },
     {
       title: $localize`Fitness & Wellness`,
       url: '/wellness',
       img: '../../assets/images/home/card_wellness_fitness.webp',
-      description: $localize`Come and relax`
+      description: $localize`Relax yourself`
     },
     {
       title: $localize`Region`,
       url: '/region',
       img: '../../assets/images/home/card_region.webp',
-      description: $localize``
+      description: $localize`Experience Bernese Alps`
     },
     {
       title: $localize`Team`,
       url: '/team',
       img: '../../assets/images/home/card_team.webp',
-      description: $localize``
+      description: $localize`Meet our team`
     },
   ];
 }
